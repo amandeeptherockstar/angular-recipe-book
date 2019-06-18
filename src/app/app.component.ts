51abf315e90297a5d83937a393759941d805124a
Amandeep Kochhar <amandeeptherockstar@gmail.com>
2019-06-18 15:27:48 +0530

    Ingredient Add and Remove Bug Remaining

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'recipe-book';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCOCXOTS_Wf-HWaglNqUamyUpVihF4TQQY',
+      authDomain: 'recipe-book-3154d.firebaseapp.com',
+      databaseURL: 'https://recipe-book-3154d.firebaseio.com',
+      projectId: 'recipe-book-3154d',
+      storageBucket: 'recipe-book-3154d.appspot.com',
+      messagingSenderId: '852397948214'
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+// import { RecipesModule } from './recipes/recipes.module';
+import { SharedModule } from './shared/shared.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+// import { RecipeService } from './recipes/recipes.service';
+import { CoreModule } from './core/core.module';
+
+// reducers
+import { StoreModule } from '@ngrx/store';
+import { reducers } from './store/app.reducers';
+// effects
+import { EffectsModule } from '@ngrx/effects';
+import { AuthEffects } from './auth/store/auth.effects';
+// router store module
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+// devtools
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // ReactiveFormsModule,
+    HttpClientModule,
+    SharedModule,
+    ShoppingListModule,
+    AuthModule,
+    // Core module has home and header components
+    CoreModule,
+    // order is important here recipesmodule must preceed approutingmodule
+    // eager loading of RecipesModule by importing it here
+    // RecipesModule,
+    AppRoutingModule,
+    // register reducers
+    StoreModule.forRoot(reducers),
+    // register Effects
+    EffectsModule.forRoot([AuthEffects]),
+    StoreRouterConnectingModule,
+    // if environment is production dont use devtools
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
-  providers: [],
+  providers: [
+    // RecipeService,
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
